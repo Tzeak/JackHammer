cdf6ec851c4338869e43ec54f5bade41f56b4373
Roshan Ramankutty <rramankutty@scu.edu>
2014-12-01 02:38:57 -0800

    Initial Commit

---
+++ JackHammer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JackHammer
+//
+//  Created by Roshan Ramankutty on 12/1/14.
+//  Copyright (c) 2014 Roshan Ramankutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JackHammer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JackHammer
+//
+//  Created by Roshan Ramankutty on 12/1/14.
+//  Copyright (c) 2014 Roshan Ramankutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
